64788c8151c33fbc6b1299b47c9731c0abe6d2a4
Rohit Kumar <rohit9075@gmail.com>
2018-10-04 16:51:54 +0530

    project Updates

--- app/src/main/java/com/example/rohit/sharedpreferencesexample/MainActivity.java
+++ app/src/main/java/com/example/rohit/sharedpreferencesexample/MainActivity.java
@@ -12,8 +12,8 @@ public class MainActivity extends Activity {
     EditText name;
     EditText email;
     public static final String mypreference = "mySharedPreference";
-    public static final String Name = "name";
-    public static final String Email = "email";
+    public static final String NameKey = "name";
+    public static final String EmailKey = "email";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,24 +21,27 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         name = findViewById(R.id.etName);
         email = findViewById(R.id.etEmail);
+
         sharedpreferences = getSharedPreferences(mypreference,
                 Context.MODE_PRIVATE);
 
 // checking for name and email id in the sharedpreferenc file
-        if (sharedpreferences.contains(Name)) {
-            name.setText(sharedpreferences.getString(Name, ""));
+        if (sharedpreferences.contains(NameKey)) {
+            name.setText(sharedpreferences.getString(NameKey, ""));
         }
-        if (sharedpreferences.contains(Email)) {
-            email.setText(sharedpreferences.getString(Email, ""));
+        if (sharedpreferences.contains(EmailKey)) {
+            email.setText(sharedpreferences.getString(EmailKey, ""));
             }
     }
 
     public void Save(View view) {
-        String n = name.getText().toString();
-        String e = email.getText().toString();
+
+        String stringName = name.getText().toString();
+        String stirngEmail = email.getText().toString();
+
         SharedPreferences.Editor editor = sharedpreferences.edit();
-        editor.putString(Name, n);
-        editor.putString(Email, e);
+        editor.putString(NameKey, stringName);
+        editor.putString(EmailKey, stirngEmail);
         editor.apply();
     }
 
@@ -52,16 +55,18 @@ public class MainActivity extends Activity {
     }
 
     public void Get(View view) {
+
         name =  findViewById(R.id.etName);
         email = findViewById(R.id.etEmail);
+
         sharedpreferences = getSharedPreferences(mypreference,
                 Context.MODE_PRIVATE);
 // checking for name and id in the shared Preference file
-        if (sharedpreferences.contains(Name)) {
-            name.setText(sharedpreferences.getString(Name, ""));
+        if (sharedpreferences.contains(NameKey)) {
+            name.setText(sharedpreferences.getString(NameKey, ""));
         }
-        if (sharedpreferences.contains(Email)) {
-            email.setText(sharedpreferences.getString(Email, ""));
+        if (sharedpreferences.contains(EmailKey)) {
+            email.setText(sharedpreferences.getString(EmailKey, ""));
 
         }
     }
